6159c3bbfbb6089741318eca3e9f2bd40d01650e
jeddabahbah <jessada.pps@gmail.com>
2017-03-02 04:05:38 +0700

    add shop

---
+++ header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>MudYom</title>
+  
+  
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,700" rel="stylesheet">
+
+	<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css'>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+<body class="demo">
+      <!-- Main Navigation -->
+      <div class="my-nav">
+         <div id="mainnavigation mnav">
+            <!-- Nav -->
+            <nav class="navbar navbar-fixed-top mnav affix-top" data-spy="affix" data-offset-top="100">
+               <div class="container">
+                  <div class="row hidden-sm hidden-xs">
+                    
+                     <!-- Logo -->
+                     <div class="col-sm-8" align="left">
+                        <p class="title mnav">BAND</p>
+                     </div>
+
+                     <div class="col-sm-4">
+                        <ul class="nav navbar-nav mnav">
+                           <li><a href="shop.html">SHOP</a></li>
+                           <li><a href="#">CART</a></li>
+                           <li><a href="#">JOURNAL</a></li>
+                           <li><a href="#">LOOKBOOK</a></li>
+                           <li><a href="#">CONTACT</a></li>
+
+                        </ul>
+                     </div>
+                     <!-- Links 02 --> 
+                  </div>
+                  <!-- /.row --> 
+               </div>
+               <!-- /.container --> 
+               <!-- Nav for small screens -->
+               <div class="container-fluid no-padding">
+                  <div class="visible-sm visible-xs">
+                     <!-- Navbar Header -->
+                     <div class="navbar-header mnav">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar_collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar first"></span> <span class="icon-bar hide-bar"></span> <span class="icon-bar last"></span> </button>
+                        <a class="navbar-brand mnav" href="#" id="mynavbar-sm mnav">BAND</a> 
+                     </div>
+                     <!-- /.Navbar Header --> 
+                     <!-- Navbar Collapse -->
+                     <div class="collapse navbar-collapse" id="navbar_collapse">
+                        <ul class="nav navbar-nav navbar-right mnav">
+                 
+                           <li><a href="#">SHOP</a></li>
+                           <li><a href="#">CART</a></li>
+                           <li><a href="#">JOURNAL</a></li>
+                           <li><a href="#">LOOKBOOK</a></li>
+                           <li><a href="#">CONTACT</a></li>
+
+                        </ul>
+                     </div>
+                     <!-- /.Navbar Collapse --> 
+                  </div>
+               </div>
+               <!-- /.container-fluid --> 
+               <!-- /.Nav for small screens --> 
+            </nav>
+            <!-- /.Nav --> 
+         </div>
+      </div>
+      <!-- /.Main Navigation --> 
+      <!-- Fullwidth Header --> 
+      <!-- Intro Header -->
